36e7f34769a6df4688fe6d4926b2dbf5d17792e7
DH-HUB <djerha@live.fr>
2024-09-19 16:39:25 +0200

    programme

--- src/data/pizzas.ts
+++ src/data/pizzas.ts
@@ -1,6 +1,12 @@
 import pizzasData from '../../data/pizzas.json'; 
 import { Pizza } from '../types/pizza-types';    
 
+<<<<<<< HEAD
 const pizzas: Pizza[] = pizzasData as Pizza[];  
 
 export { pizzas };
+=======
+const pizzas: Pizza[] = pizzasData;  // Conv avec le type Pizza
+
+export { pizzas };
+>>>>>>> 22b043c (programme)
